d5bb566845d2d60e862c27f8cd42086f0dcd7092
Ladrillo <gabriel@cabrejas.net>
2021-10-28 12:47:46 -0400

    wip

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Web 47 rocks!</h1>
+  `)
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
+
+
+function cohortRocks() { // eslint-disable-line
+  console.log('arguments:', process.argv.slice(2))
+  console.log(`web ${process.argv[2]} rocks`) // fix it using process.argv !
+}
+function cohortRocksEnv() {
+  console.log(`Home folder is "${process.env.HOME}"`)
+  console.log(`My shell is "${process.env.SHELL}"`)
+  console.log(`In my environment FOO is "${process.env.FOO}"`)
+  console.log(`My super secret comes from "${process.env.SUPER_SECRET}"`)
+}
+cohortRocksEnv()
